4f7ec68e4ead5bb7320d84eabb1dd0bdec95719c
aimeric-landou <aimeric.landou65@gmail.com>
2024-03-05 19:43:02 +0100

    AIMERIC randomCone debug3

--- PWGJE/Tasks/jetfinderQA.cxx
+++ PWGJE/Tasks/jetfinderQA.cxx
@@ -964,8 +964,11 @@ struct JetFinderQATask {
   }
   PROCESS_SWITCH(JetFinderQATask, processRho, "QA for rho-area subtracted jets", false);
 
-  void processRandomCone(soa::Filtered<soa::Join<JetCollisions, aod::BkgChargedRhos>>::iterator const& collision, soa::Join<aod::ChargedJets, aod::ChargedJetConstituents> const& jets, JetTracks const& tracks)
+  void processRandomCone(soa::Filtered<soa::Join<JetCollisions, aod::BkgChargedRhos>>::iterator const& collision, soa::Join<aod::ChargedEventWiseSubtractedJets, aod::ChargedEventWiseSubtractedJetConstituents> const& jets, JetTracks const& tracks)
   {
+    if (jets.size()==0){
+      return;
+    }
     LOGF(info, "00000000000000000000000000000000000000000000000000000000000000000000000000000000 RandomCone 0 00000000000000000000000000000000000000000000000000000000000000000000000000000000");
     TRandom3 randomNumber(0);
     float randomConeEta = randomNumber.Uniform(trackEtaMin + randomConeR, trackEtaMax - randomConeR);
